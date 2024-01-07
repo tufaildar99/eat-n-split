948cc7a6ca9cb5fbd2d6c30ddf42bea97e04cde5
Tufail Dar <tufaildar2016@gmail.com>
2024-01-07 10:21:31 +0530

    Add friend functionality and toggle button

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [
   {
     id: 118836,
@@ -20,15 +22,31 @@ const initialFriends = [
 ];
 
 export default function App() {
+  const [showAddFriend, setShowAddFriend] = useState(false);
+
+  function handleShowAddFriend() {
+    setShowAddFriend((show) => !showAddFriend);
+  }
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendsList />
-        <FormAddFriend />
-        <Button>Add Friend</Button>
+        {showAddFriend && <FormAddFriend />}
+        <Button onClick={handleShowAddFriend}>
+          {showAddFriend ? "Close" : "Add Friend"}
+        </Button>
       </div>
       <FormSplitBill />
     </div>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
@@ -64,10 +82,6 @@ function Friend({ friend }) {
   );
 }
 
-function Button({ children }) {
-  return <button className="button">{children}</button>;
-}
-
 function FormAddFriend() {
   return (
     <form className="form-add-friend">
@@ -98,8 +112,8 @@ function FormSplitBill() {
 
       <label>💸 Who's paying the bill?</label>
       <select>
-        <option>You</option>
-        <option>X</option>
+        <option value="user">You</option>
+        <option value="friend">X</option>
       </select>
       <Button>Split the bill</Button>
     </form>
